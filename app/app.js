674a26e62a1966321c6b77043ac32809f6547087
pkalro <pkalro05@gmail.com>
2016-02-19 17:44:20 +0530

    First commit

--- app/app.js
+++ app/app.js
@@ -3,10 +3,144 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+   'ui.bootstrap',
+   'ngAnimate'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+	$routeProvider.when('/studentattendance',{
+          templateUrl:'templates/studentattendance.html',
+          controller:'StudentAttendanceCtrl'
+	})
+	$routeProvider.when('/uploadassignments',{
+		templateUrl:'templates/uploadassignment.html',
+		controller:'UploadAssignmentCtrl'
+	})
+	$routeProvider.when('/updatemarks',{
+		templateUrl:'templates/updatemarks.html',
+		controller:'UpdateMarksCtrl'
+	})
+	$routeProvider.when('/viewmarks',{
+		templateUrl:'templates/viewmarks.html',
+		controller:'ViewMarksCtrl'
+	})
+	$routeProvider.when('/updateattendance',{
+		templateUrl:'templates/updateattendance.html',
+		controller:'UpdateAttendanceCtrl'
+	})
+  $routeProvider.when('/',{
+    templateUrl:'templates/loginpage.html',
+    controller:'ModalDemoCtrl'
+  })
+  $routeProvider.when('/home',{
+    templateUrl:'templates/home.html',
+    controller:'HomeCtrl'
+  })
+  $routeProvider.when('/hometest',{
+    templateUrl:'templates/hometest.html',
+    controller:'HomeCtrl'
+  })
+  $routeProvider.otherwise({redirectTo: '/hometest'});
+}])
+.controller('StudentAttendanceCtrl',function($scope){
+    
+
+    
+})
+.controller('HomeCtrl',function($scope){
+    
+
+    
+})
+.controller('ViewMarksCtrl',function($scope){
+
+})
+.controller('UpdateMarksCtrl',function($scope){
+
+})
+.controller('UploadAssignmentCtrl',function($scope){
+
+})
+.controller('UpdateAttendanceCtrl',function($scope,$http){
+	  $scope.usn_list = {};
+        $http.get('http://localhost:80/getusn.php?subject_code=10cs71')
+          .then(function(result){
+          	   //console.log(result.data);
+          	   
+               $scope.usn_list = result.data;
+               console.log($scope.usn_list[0].usn);
+          });
+   $scope.usnselected = function()
+   {
+   	   var option = confirm("Are you sure you want to submit?");
+   	   console.log(option);
+   	   $scope.selected_usn = [];
+   	   var k=0;
+   	   
+   	  for(var index = 0;index<$scope.usn_list.length;index++)
+   	  {
+   	  	if(document.getElementById($scope.usn_list[index].usn).checked)
+              $scope.selected_usn[k++]= $scope.usn_list[index].usn;
+   	  }
+   	  console.log(JSON.stringify($scope.selected_usn));
+   	  if(option)
+   	  {
+   	  $http.get('http://localhost:80/update_attendance.php?usn_list='+JSON.stringify($scope.selected_usn))
+          .then(function(result){
+          	   //console.log(result.data);
+          	   console.log(result);
+          });
+      }
+      else
+      {
+
+      }
+   }
+})
+.controller('ModalDemoCtrl', function ($scope, $uibModal, $log) {
+
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.animationsEnabled = true;
+
+  $scope.open = function (size) {
+
+    var modalInstance = $uibModal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: 'sm',
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+
+})
+.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items,$location) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $location.path('/hometest');
+     $uibModalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+})
